2e6cb037e0ad221a588b446072162ec475018973
Marcin Kałat <marcin.kalat@hotmail.com>
2024-09-10 11:39:39 +0200

    added restfull apis, and etc

---
+++ app/Http/Controllers/ArticleApi.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article as ModelsArticle;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class ArticleApi extends Controller
+{
+    public function list(Request $req){
+        $articles = ModelsArticle::all();
+        return new JsonResponse(['result' => $articles],200);
+    }
+
+    public function get(Request $req){
+        $data = json_decode($req->payload, true);
+        $rules = [
+            'id' => 'required|unique:article',
+        ];
+
+        $validator = Validator::make($data, $rules);
+        if ($validator->passes()) {
+            $article = ModelsArticle::find($data['id']);
+            return new JsonResponse(['result' => $article],200);
+        } else {
+            return new JsonResponse(['message' => $validator->errors()->all()]);
+        }
+    }
+    public function add(Request $req){
+        $data = json_decode($req->payload, true);
+
+        $rules = [
+            'title' => 'required|max:255',
+            'body' => 'required'
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->passes()) {
+            $article = new ModelsArticle;
+            $article->title = $data['title'];
+            $article->body = $data['body'];
+            $article->user = Auth::user();
+            $article->save();
+            return new JsonResponse(['result' => $article],200);
+        } else {
+            return new JsonResponse(['message' => $validator->errors()->all()]);
+        }
+    }
+    public function delete(Request $req){
+        $data = json_decode($req->payload, true);
+
+        $rules = [
+            'id' => 'required|unique:article',
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->passes()) {
+            $article = ModelsArticle::find($data['id']);
+            $article->delete();
+            return new JsonResponse(['result' => $req->id],200);
+        } else {
+            return new JsonResponse(['message' => $validator->errors()->all()]);
+        }
+    }
+    public function update(Request $req){
+        $data = json_decode($req->payload, true);
+
+        $rules = [
+            'id' => 'required|unique:article',
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ];
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->passes()) {
+            $article = ModelsArticle::find($data['id']);
+            $article->update([
+                'title' => $data['titla'],
+                'body' => $data['body'],
+            ]);
+            return new JsonResponse(['result' => $article],200);
+        } else {
+            return new JsonResponse(['message' => $validator->errors()->all()]);
+        }
+    }
+
+    public function searchByTitle(Request $req){
+        $article = ModelsArticle::where('title', 'like', '%'.$req->title.'%')->get();
+        return new JsonResponse(['result' => $article],200);
+    }
+
+    public function searchByBody(Request $req){
+        $article = ModelsArticle::where('body', 'like', '%'.$req->body.'%')->get();
+        return new JsonResponse(['result' => $article],200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Article;
+use App\Http\Controllers\ArticleApi;
 use App\Http\Middleware\UserIsAuthenticated;
 use App\Http\Middleware\UserIsAuthor;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/api/article/list', [Article::class, 'list']);
-Route::get('/api/article/{id}', [Article::class, 'get']);
-Route::post('/api/article/add/{id}', [Article::class, 'add'])->middleware(UserIsAuthenticated::class);
-Route::post('/api/article/update/{id}', [Article::class, 'update'])->middleware([UserIsAuthenticated::class, UserIsAuthor::class]);
-Route::post('/api/article/delete/{id}', [Article::class, 'delete'])->middleware([UserIsAuthenticated::class, UserIsAuthor::class]);;
+Route::get('/api/article/list', [ArticleApi::class, 'list']);
+Route::get('/api/article/{id}', [ArticleApi::class, 'get']);
+Route::post('/api/article/add/{id}', [ArticleApi::class, 'add'])->middleware(UserIsAuthenticated::class);
+Route::put('/api/article/update/{id}', [ArticleApi::class, 'update'])->middleware(UserIsAuthor::class);
+Route::delete('/api/article/delete/{id}', [ArticleApi::class, 'delete'])->middleware(UserIsAuthor::class);;
